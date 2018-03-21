5862d17a75389fcfee2e8e8073175bb8ff345ac3
Phpdots-Ashok <ashok.kumar@indusface.com>
2018-03-21 10:33:41 +0530

    Market API changed

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -37,14 +37,25 @@ class ApiController extends Controller
     public function TopGainer(Request $request, $market_id = null)
     {
         $market_data  = "CALL select_Top_Gainer(".$market_id.")";
-        $data = callCustomSP($market_data);
-        $returnData['top_gainer'] = $data;
-
+        $gainer_data = callCustomSP($market_data);
+        $returnData['top_gainer'] = $gainer_data;
+        
+        if(isset($gainer_data[0])){
+            $security_id = $gainer_data[0]['id']; $month_id = 3; $benchmark = '';
+            $market_data  = "CALL Select_Historical_Data(".$security_id.", ".$month_id.", '".$benchmark."')";
+            $History_data = callCustomSP($market_data);
+            $returnData['gainer_history_data'] = $History_data;
+        }
+        
         $market_data  = "CALL select_Top_Loser(".$market_id.")";
-        $data = callCustomSP($market_data);
-        $returnData['top_loser'] = $data;
-
-        $returnData['line_graph_data'] = [];
+        $loser_data = callCustomSP($market_data);
+        $returnData['top_loser'] = $loser_data;
+        if(isset($loser_data[0])){
+            $security_id = $loser_data[0]['id']; $month_id = 3; $benchmark = '';
+            $market_data  = "CALL Select_Historical_Data(".$security_id.", ".$month_id.", '".$benchmark."')";
+            $History_data = callCustomSP($market_data);
+            $returnData['loser_history_data'] = $History_data;
+        }
 
         return ['status' => 1,'msg' => "OK", "data" => $returnData];
     }
