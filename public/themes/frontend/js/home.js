9a258f5d763de726de7688fd9b3f1d8485b4afae
Phpdots-Ashok <ashok.kumar@indusface.com>
2018-03-27 16:30:43 +0530

    home

--- public/themes/frontend/js/home.js
+++ public/themes/frontend/js/home.js
@@ -1,3 +1,10 @@
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(lineChart);
+
+google.charts.load('current', {'packages': ['corechart']});
+
+google.charts.load('current', {'packages': ['bar']});
+
 $(document).ready(function (){
       generateHomeLineGraph();
 });
@@ -14,8 +21,23 @@ function generateHomeLineGraph()
         data: {security_id: global_line_graph_id, month_id: val},
         success: function (result)
         {
-            console.log(result);
-            lineChart(result.data.history_data);
+            var formatedData = [];
+            var data_values = result.data.history_data;
+            // console.log(data_values);
+            var counter = data_values.length;
+            if (counter > 0)
+            {
+                $columnTitle = "LineChart";
+                // alert("Title: " + $columnTitle); 
+                formatedData.push([$columnTitle, $columnTitle]);
+                var j = 1;
+                for (var i in data_values)
+                {
+                    formatedData.push([data_values[i]['created_format'], parseFloat(data_values[i]['last_price'])]);
+                }
+            }
+            
+            lineChart(formatedData);
         },
         error: function (error) {
             $('#AjaxLoaderDiv').fadeOut('slow');
@@ -24,9 +46,8 @@ function generateHomeLineGraph()
     });
 }
 
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(lineChart);
 function lineChart(data) {
+  // var data = google.visualization.arrayToDataTable(data);
     /*var data = google.visualization.arrayToDataTable([
       ['Day', 'index'],
       ['2004',  100],
@@ -35,7 +56,7 @@ function lineChart(data) {
       ['2007',  1210],
       ['2008',  1350]
     ]);*/
-
+  console.log(data);
     var options = {
       title: '',
       curveType: 'none',
@@ -58,8 +79,8 @@ function lineChart(data) {
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('chart_home'));
-
     chart.draw(data, options);
+
 }
 
 google.charts.setOnLoadCallback(drawChart2);
