88d7b810656a18bdb94a8b00e38c28aa7aab1047
DESKTOP-GIMQF68\vishw <vishwas.jadav@phpdots.com>
2018-04-06 10:30:09 +0530

    Search by security name in admin panel securities list.

--- resources/views/admin/securities/index.blade.php
+++ resources/views/admin/securities/index.blade.php
@@ -8,17 +8,17 @@
     <div class="container">
 
         <div class="col-md-12">
-            
-            @include($moduleViewName.".search")           
 
-            <div class="clearfix"></div>    
+            @include($moduleViewName.".search")
+
+            <div class="clearfix"></div>
             <div class="portlet box green">
                 <div class="portlet-title">
                     <div class="caption">
-                        <i class="fa fa-list"></i>{{ $page_title }}    
+                        <i class="fa fa-list"></i>{{ $page_title }}
                     </div>
                 </div>
-                <div class="portlet-body">                    
+                <div class="portlet-body">
                         <table class="table table-bordered table-striped table-condensed flip-content" id="server-side-datatables">
                             <thead>
                                 <tr>
@@ -31,24 +31,24 @@
                                    <th width="5%">Default</th>
                                    <th width="10%" data-orderable="false">Action</th>
                                 </tr>
-                            </thead>                                         
+                            </thead>
                             <tbody>
                             </tbody>
-                        </table>                                              
+                        </table>
                 </div>
-            </div>              
+            </div>
         </div>
     </div>
 </div>
-</div>            
+</div>
 @endsection
 @section('styles')
-  
+
 @endsection
 
 @section('scripts')
     <script type="text/javascript">
-    
+
 
     $(document).ready(function(){
 
@@ -67,14 +67,15 @@
             searching: false,
             ajax: {
                 "url": "{!! route($moduleRouteText.'.data') !!}",
-                "data": function ( data ) 
+                "data": function ( data )
                 {
                     data.search_cusip = $("#search-frm input[name='search_cusip']").val();
                     data.search_market = $("#search-frm select[name='search_market']").val();
                     data.search_status = $("#search-frm select[name='search_status']").val();
-                    data.search_country = $("#search-frm input[name='search_country']").val();
+					data.search_country = $("#search-frm input[name='search_country']").val();
+                    data.search_security_name = $("#search-frm input[name='search_security_name']").val();
                 }
-            },            
+            },
             "order": [[ 0, "asc" ]],
             columns: [
                 { data :'id' , name : 'id' },
@@ -86,7 +87,7 @@
                 { data: 'default', name: 'default' },
                 { data: 'action', orderable: false, searchable: false}
             ]
-        });        
+        });
     });
     </script>
 @endsection
--- resources/views/admin/securities/search.blade.php
+++ resources/views/admin/securities/search.blade.php
@@ -14,17 +14,13 @@
                     <label class="control-label">CUSIP Name</label>
                     <input type="text" value="{{ \Request::get("search_cusip") }}" class="form-control" name="search_cusip" />
                 </div>
+				<div class="col-md-4">
+					<label class="control-label">Security Name</label>
+					<input type="text" value="{{ \Request::get("search_security_name") }}" class="form-control" name="search_security_name" />
+				</div>
                 <div class="col-md-4">
                     <label class="control-label">Market Type</label>
                      {!! Form::select('search_market', [''=>'Select Market Type'] + $MarketType, null,   ['class' => 'form-control market_type']) !!}
-                </div>
-                <div class="col-md-4">
-                    <label class="control-label">Default</label>
-                    <select name="search_status" class="form-control">
-                        <option value="all">All</option>                        
-                        <option value="1" {!! \Request::get("search_status") == 1 ? 'selected="selected"':'' !!}>Yes</option>                        
-                        <option value="0" {!! \Request::get("search_status") == "0" ? 'selected="selected"':'' !!}>No</option>                        
-                    </select>
                 </div>
             </div>
             <div class="clearfix">&nbsp;</div>
@@ -33,6 +29,14 @@
                     <label class="control-label">Country</label>
                     <input type="text" value="{{ \Request::get("search_country") }}" class="form-control" name="search_country" />
                 </div>
+				<div class="col-md-4">
+					<label class="control-label">Default</label>
+					<select name="search_status" class="form-control">
+						<option value="all">All</option>
+						<option value="1" {!! \Request::get("search_status") == 1 ? 'selected="selected"':'' !!}>Yes</option>
+						<option value="0" {!! \Request::get("search_status") == "0" ? 'selected="selected"':'' !!}>No</option>
+					</select>
+				</div>
                 <div class="col-md-4">
                     <input type="submit" class="btn blue mTop25" value="Search"/>
                     &nbsp;
