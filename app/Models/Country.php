8e26f8c260db7e2e317f95e5465d01bb024f26df
rathod-akshay <rathodakshay228@gmail.com>
2018-04-11 10:31:03 +0530

    Country Module Changes

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,12 +3,25 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Country extends Model
 {
+    use Sluggable;
     protected $fillable = ['title','country_code', 'country_type'];
+    protected $table = TBL_COUNTRY;
 
-       protected $table = TBL_COUNTRY;
+    public function sluggable()
+    {
+        return 
+        [
+            'slug' => 
+            [
+                'source' => 'title',
+                'on_update' => true
+            ]
+        ];
+    }      
 
     public static function getCountryList()
     {
